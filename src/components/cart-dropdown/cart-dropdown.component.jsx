a5383ac5ec3776dbdaccc6657fca07f849cd92ec
Szebasztian Lukity <lukicseba@gmail.com>
2022-01-24 14:28:03 -0500

    added cartIcon and cartDropdown components to our application

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+  return (
+    <div className='cart-dropdown'>
+      <div className='cart-items' />
+      <CustomButton>Go To CheckOut</CustomButton>
+    </div>
+  );
+};
+
+export default CartDropdown;
